7fff70c64dcac8465e8ec82dd6d775a13688c990
jiyoung0123 <124110280+jiyoung0123@users.noreply.github.com>
2023-02-23 17:09:16 +0900

    Update P68.java
    exchange

--- day02/src/variable/P68.java
+++ day02/src/variable/P68.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class P68 {
 
 	public static void main(String[] args) {
+		String s2 = "11";
 		String s1 = "10";
 		int s2 = Integer.parseInt(s1); // JDK에서 제공하는 API의 하나.-parseInt 등등..
 		int a = 10;
